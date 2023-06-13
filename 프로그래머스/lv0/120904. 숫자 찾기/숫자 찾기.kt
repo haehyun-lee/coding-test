2f2b769401e68caca2bbac3cdcffaa68674a83dd
Haehyun Lee <bada7144@gmail.com>
2023-06-14 00:11:47 +0900

    [level 0] Title: 숫자 찾기, Time: 14.84 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.kt
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.kt
@@ -1,8 +1,3 @@
 class Solution {
-    fun solution(num: Int, k: Int): Int = num.toString()
-        .map(Char::digitToInt)
-        .indexOf(k).let {
-            if (it != -1) it + 1
-            else it
-        }
+    fun solution(num: Int, k: Int) = "-$num".indexOf(k.toString())
 }
