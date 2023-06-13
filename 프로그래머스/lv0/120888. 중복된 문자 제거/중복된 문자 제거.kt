c96ffd955224bc301048976ae483b59a0a0abc06
Haehyun Lee <bada7144@gmail.com>
2023-06-14 01:01:43 +0900

    [level 0] Title: 중복된 문자 제거, Time: 13.10 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.kt
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.kt
@@ -1,7 +1,3 @@
 class Solution {
-    fun solution(my_string: String): String {
-        var answer = mutableListOf<Char>()
-        my_string.forEach { if (answer.indexOf(it) == -1) answer.add(it) }
-        return answer.joinToString("")
-    }
+    fun solution(my_string: String): String = my_string.toList().distinct().joinToString("")
 }
