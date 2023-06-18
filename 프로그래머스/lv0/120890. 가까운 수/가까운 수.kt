a8011641cbb3c92bb35f601ea0bd29634a442564
Haehyun Lee <bada7144@gmail.com>
2023-06-19 02:23:20 +0900

    [level 0] Title: 가까운 수, Time: 19.67 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.kt
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.kt
@@ -1,26 +1,6 @@
-import kotlin.math.abs
-
 class Solution {
-    fun solution(array: IntArray, n: Int): Int {
-        var list = array.toMutableList()
-        list.add(n)
-        list.sort()
-        val nPos = list.indexOf(n)
-
-        return when (nPos) {
-            0 -> list[1]
-            list.lastIndex -> list[list.lastIndex - 1]
-            else -> {
-                val diff1 = abs(list[nPos - 1] - n)
-                val diff2 = abs(list[nPos + 1] - n)
-                if (diff1 < diff2) {
-                    list[nPos - 1]
-                } else if (diff1 > diff2) {
-                    list[nPos + 1]
-                } else {
-                    minOf(list[nPos - 1], list[nPos] + 1)
-                }
-            }
-        }
-    }
+    fun solution(array: IntArray, n: Int): Int =
+    array.sortedWith(compareBy<Int> { Math.abs(it - n) }
+        .thenBy { it })
+        .first()
 }
